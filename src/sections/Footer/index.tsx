17021aaf1f686894079a07aa8969c84afd6580b8
GustavoBrPereira <gustavobritope@gmail.com>
2022-05-06 17:09:26 -0300

    bug: image doesnt show in vercel app

--- src/sections/Footer/index.tsx
+++ src/sections/Footer/index.tsx
@@ -3,7 +3,7 @@ import { FooterComponent } from './components'
 export function Footer() {
   return (
     <FooterComponent>
-      <img src="src/assets/img/logo.jpeg" alt="" />
+      <img src="/src/assets/img/logo.jpeg" alt="" />
     </FooterComponent>
   );
 }
